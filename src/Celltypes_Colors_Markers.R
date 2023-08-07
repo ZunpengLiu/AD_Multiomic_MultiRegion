0f6abe223c842db35499760254effd072c7fd3be
ZunpengLiu <31592626+ZunpengLiu@users.noreply.github.com>
2023-08-07 00:25:11 -0400

    Aug 6

---
+++ src/Celltypes_Colors_Markers.R
@@ -0,0 +1,6 @@
+#############
+# Description: This script is used to generate the color scheme for the cell types
+
+
+# Major cell types
+major_cell_types <- c("Astrocytes", "Endothelial", "Microglia", "Neurons", "Oligodendrocytes", "OPCs")
